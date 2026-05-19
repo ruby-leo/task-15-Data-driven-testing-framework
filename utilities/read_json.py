f885a4e5cf4a878636905873eb562ffe91152114
Nithya <nithya@Nithyas-MacBook-Pro.local>
2026-05-19 07:36:03 +0530

    added comments for readability

--- utilities/read_json.py
+++ utilities/read_json.py
@@ -1,6 +1,7 @@
 import json
 import os
 
+# Loads config.json contents into a Python dictionary
 def get_config():
     current_dir = os.path.dirname(os.path.abspath(__file__))
     filepath = os.path.join(current_dir, "..", "config", "config.json")
